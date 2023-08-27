04978b6610c297e96951f0ff813634e66e9ef65f
Gobinda Goswamee <127843847+GobindaGoswamee6328@users.noreply.github.com>
2023-08-27 18:30:21 +0600

    Add files via upload

---
+++ Inner If Statement.py
@@ -0,0 +1,20 @@
+
+#It also called Nested If Statement
+
+num1 = 30
+num2 = 40
+num3 = 60
+
+if num1 > num2:
+     if num1 > num3:
+         print(num1)
+     else:
+         print(num3)
+
+if num2 > num1:
+     if num2 > num3:
+         print(num2)
+     else:
+         print(num3)
+
+
---
+++ Ternary Operator.py
@@ -0,0 +1,12 @@
+
+num1 = 50
+num2 = 20
+
+'''
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)
+'''
+
+print(num1 if num1 > num2 else num2)
