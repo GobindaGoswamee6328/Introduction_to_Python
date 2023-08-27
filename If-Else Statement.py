e65e15fc4545862b8802d50836b7185cc70264dd
Gobinda Goswamee <127843847+GobindaGoswamee6328@users.noreply.github.com>
2023-08-27 16:48:34 +0600

    Add files via upload

---
+++ If-Else Statement.py
@@ -0,0 +1,30 @@
+
+#Pass-Fail:
+
+mark = 70
+
+if mark>=33:
+    print("Pass")
+else:
+   print("Fail")
+
+
+#Even-Odd:
+
+num = 8
+
+if num %2==0:
+    print("Even")
+else:
+    print("Odd")
+
+
+#Largest_Smallest
+
+num1 = 20
+num2 = 10
+
+if num1>num2:
+    print("num1")
+else:
+    print("num2")
