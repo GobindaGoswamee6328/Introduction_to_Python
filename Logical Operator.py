d62c924a5c286b49a1d79a276832ced8d0cc3c7d
Gobinda Goswamee <127843847+GobindaGoswamee6328@users.noreply.github.com>
2023-08-27 19:45:54 +0600

    Add files via upload

---
+++ Logical Operator.py
@@ -0,0 +1,25 @@
+
+#Using And Operator:
+
+num1 = 30
+num2 = 18
+num3 = 25
+
+if num1 > num2 and  num1 > num3:
+         print(num1)
+
+elif num2 > num1 and num2 > num3:
+         print(num2)
+else:
+         print(num3)
+
+
+#Using Or Operator:
+
+ch = 'a'
+
+if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+    print("Vowel")
+
+else:
+    print("Consonant")
