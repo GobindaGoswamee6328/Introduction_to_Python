c4be93911c31dd9401ae7b3f06865219ffa3f9ea
Gobinda Goswamee <127843847+GobindaGoswamee6328@users.noreply.github.com>
2023-08-27 19:45:28 +0600

    Add files via upload

---
+++ Letter Grade Program.py
@@ -0,0 +1,24 @@
+
+mark = 86
+
+
+if 90<= mark <=100:
+    print("A+")
+elif 85<= mark <=89:
+    print("A")
+elif 80<= mark <=84:
+    print("B+")
+elif 75<= mark <=79:
+    print("B")
+elif 70<= mark <=74:
+    print("C+")
+elif 65<= mark <=69:
+    print("C")
+elif 60<= mark <=64:
+    print("D+")
+elif 50<= mark <=59:
+    print("D")
+
+
+else:
+   print("Fail")
