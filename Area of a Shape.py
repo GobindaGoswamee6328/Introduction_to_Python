1752db35794973cdaa919206a838a9c1b29e3ca3
Gobinda Goswamee <127843847+GobindaGoswamee6328@users.noreply.github.com>
2023-08-27 12:58:55 +0600

    Add files via upload

---
+++ Area of a Shape.py
@@ -0,0 +1,35 @@
+
+#Triangle:
+
+base= float(input("Enter base : "))
+height= float(input("Enter height : "))
+
+area = 0.5*base*height
+print(area)
+
+
+#Circle:
+
+radius = float(input("Enter radius : "))
+
+area = 3.1416*radius**2
+print(area)
+
+
+#Square:
+
+length = float(input("Enter a length : "))
+
+area = length**2
+print(area)
+
+
+#Rectangle:
+
+width= float(input("Enter width : "))
+height= float(input("Enter height : "))
+
+area = width*height
+print(area)
+
+
