b655621f849d72e05d4391defe8f1f3ac091a473
Gobinda Goswamee <127843847+GobindaGoswamee6328@users.noreply.github.com>
2023-08-27 16:50:10 +0600

    Add files via upload

---
+++ Formatted String & Type Function.py
@@ -0,0 +1,37 @@
+
+#Normal Formatting:
+
+num1 = 25
+num2= 35
+
+print(f"{num1} + {num2} = {num1+num2}")
+#or
+print("The sum is : ",num1+num2)
+#or
+print(num1+num2)
+
+
+#EndLine Formatting:
+
+print("Simanta Goswamee",end =" ")
+print('015658373**8')
+
+
+#Define Type:
+
+#int:
+num = 20
+print(type(num))
+
+#Float:
+num = 20.5
+print(type(num))
+
+#String:
+num = "Simran"
+print(type(num))
+
+#Boolian:
+
+print(type(True))
+
