d71c319dc6f6dcacebfdc9038b7241ad11445264
Gobinda Goswamee <127843847+GobindaGoswamee6328@users.noreply.github.com>
2023-08-28 12:17:25 +0600

    Add files via upload

---
+++ Sum of N numbers.py
@@ -0,0 +1,37 @@
+
+#Total Sum:
+
+sum = 0
+i = 1
+
+while i <= 100:
+    sum = sum + i
+    i = i + 1
+
+print(sum)
+
+
+#User Input Sum:
+
+n = int(input(" Enter the Last Number : "))
+sum = 0
+i = 1
+
+while i <= n:
+    sum = sum + i
+    i = i + 1
+
+print(sum)
+
+
+#2 + 4 + 6 + 8 +........n:
+
+n = int(input(" Enter the Last Number : "))
+sum = 0
+i = 2
+
+while i <= n:
+    sum = sum + i
+    i = i + 2
+
+print(sum)
---
+++ While Loop.py
@@ -0,0 +1,33 @@
+
+#Even:
+
+i = 2
+
+while i <= 100:
+    print(i)
+    i = i + 2
+
+print("Program End")
+
+
+#Odd:
+
+i = 1
+
+while i <= 100:
+    print(i)
+    i = i + 2
+
+print("Program End")
+
+
+#Number:
+
+i = 1
+
+while i <= 20:
+    print(i)
+    i = i + 1
+
+print("Program End")
+
