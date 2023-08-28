c0a4f9148853f79d50443094d099d16287647e1a
Gobinda Goswamee <127843847+GobindaGoswamee6328@users.noreply.github.com>
2023-08-28 21:07:12 +0600

    Add files via upload

---
+++ Range Function.py
@@ -0,0 +1,26 @@
+
+num = list(range(20))
+
+print(num)
+print(num[8])
+
+
+#Set Limit:
+
+num = list(range(8,16))
+
+print(num)
+
+
+#Odd Range:
+
+num = list(range(1,100,2))
+
+print(num)
+
+
+#Even Range:
+
+num = list(range(2,101,2))
+
+print(num)
