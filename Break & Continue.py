3a910491df3f9aa8325ada99fa2ec4f1c1fef223
Gobinda Goswamee <127843847+GobindaGoswamee6328@users.noreply.github.com>
2023-08-28 13:21:30 +0600

    Add files via upload

---
+++ Break & Continue.py
@@ -0,0 +1,25 @@
+
+#Break:
+
+i = 1
+
+while i <= 100:
+    if i == 20:
+        break
+    print(i)
+    i = i + 1
+
+print("Hello")
+
+
+#Continue:
+
+i = 1
+
+while i <= 100:
+    if i == 20:
+        break
+    print(i)
+    i = i + 1
+
+print("Hello")
