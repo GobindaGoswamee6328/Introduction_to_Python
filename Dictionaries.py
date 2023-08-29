5e78c2bda8b71f8c00c6ae31c501cf5d1bdefd26
Gobinda Goswamee <127843847+GobindaGoswamee6328@users.noreply.github.com>
2023-08-29 14:15:48 +0600

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,11 @@
+
+studentId = {
+
+    "1" : "Gobinda",
+    "2" : "Simanta",
+    "3" : "Samrin",
+    "4" : "Joy",
+}
+
+print(studentId["1"])
+print(studentId.get("5","Not a Valid key"))
---
+++ Matrix.py
@@ -0,0 +1,22 @@
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+]
+
+matrix [1][1] = 7
+print(matrix[1][1])
+
+
+#Using Nasted For-Loop:
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+]
+
+for row in matrix:
+    for col in row:
+
+        print(col)
+
